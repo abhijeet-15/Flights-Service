ed0a699e2af0dfd2e24036c8ebb5f895380e67c2
Abhijeet Singh <abhijeet.hcs@gmail.com>
2023-05-13 22:28:39 +0530

    Added Flight Model with forein key constraint defined during model definition
    ```
    npx sequelize model:generate --name Flight --attributes flightNumber:string,airplaneId:integer,departureAirportId:integer,arrivalAirportId:integer,arrivalTime:date,departureTime:date,price:integer,boardingGate:string,totalSeats:integer
    
    ```

--- src/index.js
+++ src/index.js
@@ -13,6 +13,6 @@ app.use(express.urlencoded({extended: true}));
  
 app.use('/api', apiRoutes);
 
-app.listen(ServerConfig.PORT, async () => {
+app.listen(ServerConfig.PORT, () => {
     console.log(`Successfully started the server on the PORT : ${ServerConfig.PORT}`);
 });
