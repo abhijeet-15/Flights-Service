037d13fa022190a2dc09df2d76547b881e4f903f
Abhijeet Singh <abhijeet.hcs@gmail.com>
2023-05-13 10:58:45 +0530

    Added middleware for create city api

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -2,13 +2,16 @@ const express = require('express');
 
 const { InfoController } = require('../../controllers');
 
-const airplaneRoute = require('./airplane-route');
+const airplaneRoutes = require('./airplane-routes');
+const cityRoutes = require('./city-routes');
 
 const router = express.Router();
 
-router.use('/airplanes', airplaneRoute);
+router.use('/airplanes', airplaneRoutes);
+router.use('/cities', cityRoutes);
+
 
 router.get('/info', InfoController.info);
 
 
-module.exports =  router;
+module.exports = router;
