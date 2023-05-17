5a7719b4643f124487ed63164b7e42d8695ba864
Abhijeet Singh <abhijeet.hcs@gmail.com>
2023-05-17 18:54:04 +0530

    FIX: Airport Middleware now checks for all the mandatory fields and returns all the missing fields if there is one

--- src/middlewares/airport-middleware.js
+++ src/middlewares/airport-middleware.js
@@ -5,28 +5,24 @@ const AppError = require('../utils/errors/app-error');
 
 
 function validateCreateRequest(req, res, next) {
-    if(!req.body.name) {
 
-        ErrorResponse.message = 'Something went wrong while creating the airport';
-        ErrorResponse.error = new AppError(['Name not found in the incoming request in the correct form'], StatusCodes.BAD_REQUEST);
-        return res
-                .status(StatusCodes.BAD_REQUEST)
-                .json(ErrorResponse);
+    const missingFields = [];
+    
+    if(!req.body.name) {
+        missingFields.push('name');
     }
 
     if(!req.body.code) {
-
-        ErrorResponse.message = 'Something went wrong while creating the airport';
-        ErrorResponse.error = new AppError(['Airport code not found in the incoming request in the correct form'], StatusCodes.BAD_REQUEST);
-        return res
-                .status(StatusCodes.BAD_REQUEST)
-                .json(ErrorResponse);
+        missingFields.push('code');
     }
 
     if(!req.body.cityId) {
+        missingFields.push('cityId');
+    }
 
+    if(missingFields?.length > 0) {
         ErrorResponse.message = 'Something went wrong while creating the airport';
-        ErrorResponse.error = new AppError(['CityId not found in the incoming request in the correct form'], StatusCodes.BAD_REQUEST);
+        ErrorResponse.error = new AppError([` ${missingFields.toString()} not found in the incoming request in the correct form`], StatusCodes.BAD_REQUEST);
         return res
                 .status(StatusCodes.BAD_REQUEST)
                 .json(ErrorResponse);
