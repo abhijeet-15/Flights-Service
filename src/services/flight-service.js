aff48c08e9c69e446d71c1ebd72b9dfba2a333ad
Abhijeet Singh <abhijeet.hcs@gmail.com>
2023-05-30 16:56:24 +0530

    Added GET flight api

--- src/services/flight-service.js
+++ src/services/flight-service.js
@@ -71,7 +71,23 @@ async function getAllFlights(query) {
     }
 }
 
+async function getFlight(id) {
+    try {
+        const flight = await flightRepository.get(id);
+        return flight;
+    }
+
+    catch(error) {
+        if(error.statusCode == StatusCodes.NOT_FOUND) {
+            throw new AppError('The flight you requested does not exits', error?.statusCode);
+        }
+        throw new AppError('Cannot ftech data of the flight ',
+            StatusCodes.INTERNAL_SERVER_ERROR);
+    }
+}
+
 module.exports = {
     createFlight,
-    getAllFlights
+    getAllFlights,
+    getFlight
 }
